2cb33af315097a3985929d24c9da183ac62baf18
Konrad <Synowiec400@wp.pl>
2022-12-06 04:46:49 +0100

    form validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// timer
+
 let btnList = document.querySelectorAll('.like-btn');
 btnList.forEach(e => e.addEventListener('click', t => {
     let element = t.target.classList;
@@ -8,3 +10,22 @@ btnList.forEach(e => e.addEventListener('click', t => {
         element.add('active')
     }
 }))
+
+
+// form validation
+let email = document.getElementById('nl-email');
+let phone = document.getElementById('nl-phone');
+
+email.addEventListener('focusout', e => {
+    let value = e.target.value;
+    value.toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) === null ? e.target.classList.add('active') : e.target.classList.remove('active')
+})
+
+phone.addEventListener('focusout', e => {
+    let value = e.target.value;
+    value.toLowerCase()
+    .match('^[0-9]*$') === null || value === '' ? e.target.classList.add('active') : e.target.classList.remove('active')
+})
